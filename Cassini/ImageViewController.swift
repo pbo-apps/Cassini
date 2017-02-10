87a4779c2f3aadd9641e20946a52fc7a237b0d20
Pete Bounford <petebounford@gmail.com>
2017-02-10 09:34:05 +0100

    Replace ViewController with ImageViewController with URL as model

---
+++ Cassini/ImageViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ImageViewController.swift
+//  Cassini
+//
+//  Created by Pete Bounford on 10/02/2017.
+//  Copyright © 2017 PBO Apps. All rights reserved.
+//
+
+import UIKit
+
+class ImageViewController: UIViewController {
+
+    // MARK: - Model
+    var imageURL: NSURL?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+}
