83ae0ce48cfa16c158b1c2ce3690ef8d92dbe449
Pete Bounford <petebounford@gmail.com>
2017-02-10 16:01:01 +0100

    Only show Stanford image if no other image is loaded

--- Cassini/ImageViewController.swift
+++ Cassini/ImageViewController.swift
@@ -49,7 +49,9 @@ class ImageViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         scrollView.addSubview(imageView)
-        imageURL = NSURL(string: DemoURL.Stanford)
+        if imageURL == nil {
+            imageURL = NSURL(string: DemoURL.Stanford)
+        }
     }
     
 }
