7febeda9385ba0e856d9939008e8b51272563926
Pete Bounford <petebounford@gmail.com>
2017-02-10 15:45:51 +0100

    Add split view with buttons which can segue to the image view

---
+++ Cassini/CassiniViewController.swift
@@ -0,0 +1,18 @@
+//
+//  CassiniViewController.swift
+//  Cassini
+//
+//  Created by Pete Bounford on 10/02/2017.
+//  Copyright © 2017 PBO Apps. All rights reserved.
+//
+
+import UIKit
+
+class CassiniViewController: UIViewController {
+    
+    // MARK: - Navigation
+
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        
+    }
+}
