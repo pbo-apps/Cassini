8f6aaaead8bd9376a19736111dbce61f245d594a
Pete Bounford <petebounford@gmail.com>
2017-02-13 15:26:25 +0100

    Prevent UI from becoming unresponsive while image is loading by using multi-threading

--- Cassini/ImageViewController.swift
+++ Cassini/ImageViewController.swift
@@ -14,17 +14,36 @@ class ImageViewController: UIViewController, UIScrollViewDelegate {
     var imageURL: NSURL? {
         didSet {
             image = nil
-            fetchImage()
+            // This is a pretty reliable way of checking if the view is on screen
+            // The view will only be in a window if it's actually on the device's screen
+            if view.window != nil {
+                fetchImage()
+            }
         }
     }
     
     private func fetchImage() {
         if let url = imageURL {
-            if let imageData = NSData(contentsOf: url as URL) {
-                image = UIImage(data: imageData as Data)
+            spinner?.startAnimating()
+            DispatchQueue.global(qos: DispatchQoS.QoSClass.userInitiated).async {
+                let contentsOfURL = NSData(contentsOf: url as URL)
+                DispatchQueue.main.async { [weak weakSelf = self] in
+                    if url == weakSelf?.imageURL {
+                        if let imageData = contentsOfURL {
+                            weakSelf?.image = UIImage(data: imageData as Data)
+                        } else {
+                            weakSelf?.spinner?.stopAnimating()
+                        }
+                    } else {
+                        print("ignored data returned from url \(url)")
+                    }
+                }
+                
             }
         }
     }
+    
+    @IBOutlet weak var spinner: UIActivityIndicatorView!
     
     @IBOutlet weak var scrollView: UIScrollView! {
         didSet {
@@ -50,6 +69,7 @@ class ImageViewController: UIViewController, UIScrollViewDelegate {
             imageView.sizeToFit()
             // scrollView is an outlet, so we need to allow for case where this is nil (i.e. image setting is happening when someone is preparing this MVC to be segued to)
             scrollView?.contentSize = imageView.frame.size
+            spinner?.stopAnimating()
         }
     }
     
@@ -58,6 +78,13 @@ class ImageViewController: UIViewController, UIScrollViewDelegate {
         scrollView.addSubview(imageView)
         if imageURL == nil {
             imageURL = NSURL(string: DemoURL.Stanford)
+        }
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        if image == nil {
+            fetchImage()
         }
     }
     
